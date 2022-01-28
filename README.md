8e7f30540d5bebce4a1df0718d7224d7da7b3753
lukyanets-ai <lukyanets_ai@groupbwt.com>
2022-01-28 17:59:10 +0200

    feat: add correct read songs start, change readmy, add env.example

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Aeternum telegramBot
 
+## Run project
+* poetry install
+* set your env data
+* create database locally
+* alembic upgrade head
+
 ## Send links by text using fuzzy search
 
 # Features
--- default_data/read_songs.py
+++ default_data/read_songs.py
@@ -3,5 +3,11 @@ import csv
 file = open('default_songs.csv', encoding="utf-8")
 csvreader = csv.reader(file)
 
+
+def vaild_length():
+    return 3
+
+
 for row in csvreader:
-    print(len(row))
+    if len(row) != vaild_length():
+        raise RuntimeError(f'Length row is not valid \n {row}')
