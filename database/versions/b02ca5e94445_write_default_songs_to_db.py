91bf55fbfa44bc3ecf201aa3bb5b2a536b8a78b9
lukyanets-ai <lukyanets_ai@groupbwt.com>
2022-02-09 15:13:33 +0200

    fix: correct add to database default_songs

--- database/versions/b02ca5e94445_write_default_songs_to_db.py
+++ database/versions/b02ca5e94445_write_default_songs_to_db.py
@@ -20,7 +20,7 @@ down_revision = 'df416969bb3c'
 branch_labels = None
 depends_on = None
 
-file = open('default_data/default_songs.csv', encoding="utf-8")
+file = open('default_data/songs.csv', encoding="utf-8")
 csvreader = csv.reader(file)
 engine = sa.create_engine(mysql_connection_string())
 
@@ -28,9 +28,14 @@ engine = sa.create_engine(mysql_connection_string())
 def upgrade():
     with engine.connect() as connection:
         for row in csvreader:
+            if not row:
+                continue
+            keywords = row[0].lower()
+            if len(row) == 3:
+                keywords = f'{keywords}.{row[2]}'
             song_dict = {'name': row[0],
                          'url': row[1],
-                         'keywords': row[2].lower()}
+                         'keywords': keywords}
 
             inseted_stmt = insert(Song)
             stmt = inseted_stmt.on_duplicate_key_update(url=song_dict.get('url')).values(song_dict)
--- default_data/read_songs.py
+++ default_data/read_songs.py
@@ -1,13 +1,15 @@
 import csv
 
-file = open('default_songs.csv', encoding="utf-8")
+file = open('songs.csv', encoding="utf-8")
 csvreader = csv.reader(file)
 
 
 def vaild_length():
-    return 3
+    return 2
 
 
 for row in csvreader:
-    if len(row) != vaild_length():
+    if not row:
+        continue
+    if len(row) < vaild_length():
         raise RuntimeError(f'Length row is not valid \n {row}')
